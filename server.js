3f02fb036f6a499f39adb3991b19e57ca2a36b1e
ulvuu <musicftwlive@gmail.com>
2024-03-26 21:51:08 -0400

    Added new features, need to fix much more

--- server.js
+++ server.js
@@ -1,5 +1,36 @@
-const server = require('./lib/server').server;
+const localserver = require('./lib/server').server;
+const http = require('http');
+const socketIo = require('socket.io')
 
 const port = process.env.PORT || 3010;
 
-server.run(port);
+localserver.run(port);
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<h1>Hello, Socket.IO!</h1>');
+});
+
+// Integrate Socket.IO
+const io = socketIo(server);
+
+// Set up a connection event
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Example of handling a custom event
+  socket.on('myEvent', (data) => {
+    console.log(data);
+  });
+
+  // Handle the disconnection
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
